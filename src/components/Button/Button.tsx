f2ec6cbd149e70f8ba4a286fe027085411128d42
Vitor <vitor.menezes.amorim@gmail.com>
2023-07-28 00:03:06 -0300

    Button: created Buttons

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import {TouchableOpacity} from 'react-native';
+import {StyledContainer, StyledText} from './ButtonStyles';
+
+interface HeaderProps {
+  title: string;
+  disabled?: boolean;
+  onPress: () => void;
+  testId?: string;
+  type?: color;
+}
+
+type color = 'green' | 'blue' | 'white';
+
+const getColor = (type: color) => {
+  switch (type) {
+    case 'blue':
+      return {color: '#FFFFFF', backgroundColor: '#12C2E9'};
+    case 'green':
+      return {color: '#142995', backgroundColor: '#A5FF32'};
+    case 'white':
+      return {color: '#00000', backgroundColor: '#FFFFFF'};
+  }
+};
+
+const Button: React.FC<HeaderProps> = ({
+  title,
+  disabled = false,
+  onPress,
+  testId,
+  type,
+}) => {
+  const {backgroundColor, color} = getColor(type ?? 'white');
+  return (
+    <StyledContainer
+      disabled={disabled}
+      onPress={onPress}
+      testID={testId}
+      backgroundColor={backgroundColor}>
+      <StyledText color={disabled ? '#BBBBBB' : color}>{title}</StyledText>
+    </StyledContainer>
+  );
+};
+
+export default Button;
---
+++ src/components/Button/ButtonStyles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const StyledText = styled.Text<{color: string}>`
+  font-size: 18px;
+  line-height: 35px;
+  font-family: PT Sans;
+  color: ${props => props.color};
+`;
+
+export const StyledContainer = styled.TouchableOpacity<{
+  backgroundColor: string;
+}>`
+  padding: 10px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props =>
+    props.disabled ? '#EEEEEE ' : props.backgroundColor};
+  border-radius: 12px;
+`;
