f400c770ead6a97770344d4e404d0ecb0da443fe
Vitor <vitor.menezes.amorim@gmail.com>
2023-07-29 15:16:21 -0300

    Completed: created complete screen

---
+++ src/pages/CardSignUp/pages/Completed/Completed.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '../../../../components/Button';
+import Header from '../../../../components/Header';
+import {StyledButtonContainer} from './CompletedStyles';
+import Background from '../../../../components/Backgound';
+import {CardData} from '../../hooks/types';
+import Card from '../../../../components/Card';
+import {useRoute} from '@react-navigation/native';
+
+interface CompletedProps {
+  card: CardData;
+}
+
+const Completed: React.FC = () => {
+  const route = useRoute();
+  const {card} = route.params as CompletedProps;
+  return (
+    <Background>
+      <Header text="Wallet Test" />
+      <Card card={card} />
+      <StyledButtonContainer>
+        <Button
+          title="Avançar"
+          onPress={() => false}
+          type="blue"
+          testId="submitButton"
+        />
+      </StyledButtonContainer>
+    </Background>
+  );
+};
+
+export default Completed;
